da6fd7203d21b7ea30f0f13a19e32c0c9d3b98ee
Milo <92605623+MiloBlackstar@users.noreply.github.com>
2023-05-23 13:22:11 +0200

    Create header for mobile view

--- src/components/TitleSpace.jsx
+++ src/components/TitleSpace.jsx
@@ -1,7 +1,11 @@
+
+
 export default function TitleSpace() {
   return (
     <header>
-      
+      <p className="top-description">mouthwatering delicious</p>
+      <h1>Learn how to make the best <span>sushi</span> in the town!</h1>
+      <p className="sub-description">Join us for this live webinar</p>
     </header>
   )
 }
