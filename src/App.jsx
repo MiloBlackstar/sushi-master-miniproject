5e3f78255c4240883fd0e28776407c3f170d9482
Milo <92605623+MiloBlackstar@users.noreply.github.com>
2023-05-23 12:23:33 +0200

    Add main style rules and link fonts and css files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import TitleSpace from './components/TitleSpace.jsx'
+
+
+export default function App() {
+  return (
+    <div className="Page">
+      <TitleSpace />
+    </div>
+  )
+}
---
+++ src/components/TitleSpace.jsx
@@ -0,0 +1,7 @@
+export default function TitleSpace() {
+  return (
+    <header>
+      
+    </header>
+  )
+}
